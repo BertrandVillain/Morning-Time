f84c3d2c3c4bbd28e3376678a7cd4746630f4298
Solenomnom <soledad.cot@epitech.eu>
2016-01-29 22:35:57 +0100

    Adding Key classes

---
+++ Key.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+
+	public char KeyBoard;
+    public GameObject KeySprite;
+}
---
+++ KeyArray.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyArray : MonoBehaviour {
+
+    public GameObject[] array;
+}
