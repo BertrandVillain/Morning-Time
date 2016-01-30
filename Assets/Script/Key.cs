97a91f258d4e28c586ec54c0deef9aabb46fa0d8
Bertrand Villain <bertrand.villain23@gmail.com>
2016-01-30 10:35:36 +0100

    Key stuff
    Random, keys

---
+++ Assets/Script/Key.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+
+	public char KeyBoard;
+    public GameObject KeySprite;
+
+    public void displayKey()
+    {
+        KeySprite.active = true;
+    }
+}
