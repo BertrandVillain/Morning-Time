1b05989885c905615595dd8dded8967be4207a13
Bertrand Villain <bertrand.villain23@gmail.com>
2016-01-30 11:03:38 +0100

    Scripts Keys

---
+++ Les Scripts/KeyArray.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class KeyArray : MonoBehaviour
+{
+    public Key[] array ;
+
+    public Key returnKey()
+    {
+        return (array[Random.Range(0, array.Length)]);
+    }
+    
+}
